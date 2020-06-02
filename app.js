249a7e35821291069cbc65d660e6d797ee94949d
fujiichi <ichirou.fujii@gmail.com>
2020-06-02 20:44:39 +0900

    add sample routing

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var express = require('express');
 
 var app = express();
 
-app.use(function(req, res, next) {
+app.get("/",function(req, res, next) {
 	return res.send('Hello World');
+});
+
+app.get("/hoge",function(req, res, next) {
+	return res.send('hoge');
 });
 
 var server = http.createServer(app);
