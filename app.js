f081d709453354fadf6d5b321a79c55534ef3813
fujiichi <ichirou.fujii@gmail.com>
2020-06-06 21:44:54 +0900

    add image upload function

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var path = require('path');
 var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
+var fileUpload = require('express-fileupload');
 
 var Message = require('./schema/Message');
 
@@ -20,6 +21,7 @@ mongoose.connect('mongodb://localhost:27017/chatapp',
 });
 
 app.use(bodyparser());
+app.use("/image", express.static(path.join(__dirname, 'image')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -35,22 +37,34 @@ app.get("/update",function(req, res, next) {
 	return res.render('update');
 });
 
-app.get("/hoge",function(req, res, next) {
-	return res.send('hoge');
+
+app.post("/update", fileUpload(), function(req, res, next) {
+  if(req.files && req.files.image) {
+    req.files.image.mv('./image/' + req.files.image.name, function(err){
+      if(err) throw err;
+      var newMessage = new Message({
+        username: req.body.username,
+        message: req.body.message,
+        image_path: '/image/' + req.files.image.name
+      });
+      newMessage.save((err)=>{
+        if(err) throw err;
+        return res.redirect("/");
+      });
+    });
+  }else{
+    var newMessage = new Message({
+      username: req.body.username,
+      message: req.body.message
+    });
+    newMessage.save((err)=>{
+      if(err) throw err;
+      return res.redirect("/");
+    });
+  }
 });
 
-app.post("/update", function(req, res, next) {
-  var newMessage = new Message({
-    username: req.body.username,
-    message: req.body.message
-    //username: "foo",
-    //message: "bar"
-  });
-  newMessage.save((err)=>{
-    if(err) throw err;
-    return res.redirect("/");
-  });
-});
+
 
 var server = http.createServer(app);
 server.listen('3000');
--- schema/Message.js
+++ schema/Message.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var Message = mongoose.Schema({
   username: String,
   message: String,
-  date: {type: Date, default: new Date()}
+  date: {type: Date, default: new Date()},
+  image_path: String
 });
 
-module.exports = mongoose.model('Message', Message);
+module.exports = mongoose.model('Message', Message)
